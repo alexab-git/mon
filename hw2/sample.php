6a040c9a05880c09512fb2168389a9fc17b0be3a
Alex <>
2020-09-10 20:41:28 +0300

    config files added

---
+++ hw2/sample.php
@@ -0,0 +1,24 @@
+<?php
+
+function generateRandomString($length = 10)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+
+
+for($i = 0; $i < rand(1,50); $i++)
+{
+    $length= rand(10, 1024);
+
+    print(generateRandomString($length));
+}
+
+?>
+
